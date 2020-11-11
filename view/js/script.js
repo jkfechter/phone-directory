b7c3fdf2058c6fe328b915325b702a6aa7ec8257
jkfechter <jk.fechter@gmail.com>
2020-11-11 10:12:01 -0800

    Update script.js

--- view/js/script.js
+++ view/js/script.js
@@ -1,1 +1,36 @@
 // Js code
+
+//set attribute required for three fields Name, Moblie, and Email
+const nameInput = document.getElementById('name');
+nameInput.required = true;
+const mobileInput = document.getElementById('mobile');
+mobileInput.required = true;
+const emailInput = document.getElementById('email');
+emailInput.required = true;
+
+//clicking Add Contact button adds contact to table
+
+const newContact = {
+    name: nameInput.value,
+    mobile: mobileInput.value,
+    email: emailInput.value
+};
+
+const submitContactInfo = window.contactsList.push(newContact);
+const submitButton = document.getElementById('submit');
+
+submitButton.onclick(submitContactInfo);
+ 
+// name - contains only alphabets and space. less than or equal to 20 characters
+nameInput.maxlength = 20;
+nameInput.pattern="[a-zA-Z\s]+";
+
+//mobile - contains only numbers. 10 characters in length
+mobileInput.length = 20;
+mobileInput.type = 'number';
+
+//email - proper validation. 40 or less characters in length
+emailInput.maxlength = 40;
+emailInput.type = 'email';
+
+//show an error div with id 'error' if any error or empty fields
